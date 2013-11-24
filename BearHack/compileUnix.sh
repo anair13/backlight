3c6adf236cff738bbe0e1317fb0d85113798bd9b
Roy Tu <thatsnail@gmail.com>
2013-11-24 05:15:28 -0800

    gorb

--- BearHack/compileUnix.sh
+++ BearHack/compileUnix.sh
@@ -1,1 +1,2 @@
-g++ -w -g -Wall -Iinclude main.cpp -o BearHack `pkg-config gtkmm-3.0 --cflags --libs`
+#g++ -w -g -Wall -Iinclude main.cpp -o BearHack `pkg-config gtkmm-3.0 --cflags --libs` -lboost_system-mgw45-mt-1_53 -lboost_date_time-mgw45-mt-1_53 -lboost_regex-mgw45-mt-1_53
+g++ -w -g -Wall -Iinclude main.cpp -o BearHack `pkg-config gtkmm-3.0 --cflags --libs` -Llib -lboost_system
--- BearHack/include/SerialHandlerUnix.h
+++ BearHack/include/SerialHandlerUnix.h
@@ -1,47 +1,15 @@
 #ifndef SERIALHANDLER_UNIX_H
 #define SERIALHANDLER_UNIX_H
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>     // string function definitions
-#include <fcntl.h>      // File control definitions
-#include <termios.h>    // POSIX terminal control definitions
-#include <sstream>
+#include "boost/asio.hpp"
 
 class SerialHandler
 {
 public:
-	SerialHandler(std::string port, unsigned int baudRate)
+	SerialHandler(std::string port, unsigned int baudRate) : io(), serial(io, port)
 	{
-        /*
-        int serial = open(port.c_str(), O_RDWR|O_NOCTTY);
-
-        struct termios tty;
-        struct termios tty_old;
-        memset(&tty, 0, sizeof tty);
-
-        tty_old = tty;
-
-        cfsetospeed (&tty, (speed_t)baudRate);
-        cfsetispeed (&tty, (speed_t)baudRate);
-
-        tty.c_cflag &= ~PARENB;        // Make 8n1
-        tty.c_cflag     &=  ~CSTOPB;
-        tty.c_cflag     &=  ~CSIZE;
-        tty.c_cflag     |=  CS8;
-
-        tty.c_cflag     &=  ~CRTSCTS;       // no flow control
-        tty.c_cc[VMIN]      =   1;                  // read doesn't block
-        tty.c_cc[VTIME]     =   5;                  // 0.5 seconds read timeout
-        tty.c_cflag     |=  CREAD | CLOCAL;     // turn on READ & ignore ctrl lines
-
-        cfmakeraw(&tty);
-
-        tcflush(serial, TCIFLUSH );
-        */
-        //file = fopen(port.c_str(), "w");
-    }
+		serial.set_option(boost::asio::serial_port_base::baud_rate(baudRate));
+	}
 
 	void writeSerial(unsigned char r, unsigned char g, unsigned char b) {
 		unsigned char c[4];
@@ -49,21 +17,12 @@ public:
 		c[1] = r;
 		c[2] = g;
 		c[3] = b;
-        std::stringstream ss;
-        ss << "python sendColor.py " << r << " " << g << " " << b;
-        system(ss.str().c_str());
-        //write(serial, c, sizeof(c));
-        /*
-        for(int i = 0 ; i < 4; i++)
-        {
-            fprintf(file,"%d", c[i]);
-        }
-        */
-        //fclose(file);
+		boost::asio::write(serial, boost::asio::buffer(c, sizeof(c)));
 	}
 
 private:
-    //FILE* file;
+	boost::asio::io_service io;
+	boost::asio::serial_port serial;
 };
 
 #endif
