58c9ebdcd32afcca611abd34241dd1f0020547e3
Roy Tu <thatsnail@gmail.com>
2013-11-24 04:27:00 -0800

    Fuck you

---
+++ BearHack/include/SerialHandlerUnix.h
@@ -0,0 +1,52 @@
+#ifndef SERIALHANDLER_UNIX_H
+#define SERIALHANDLER_UNIX_H
+
+#include <string.h>     // string function definitions
+#include <fcntl.h>      // File control definitions
+#include <termios.h>    // POSIX terminal control definitions
+
+class SerialHandler
+{
+public:
+	SerialHandler(std::string port, unsigned int baudRate)
+	{
+        int serial = open(port.c_str(), O_RDWR|O_NOCTTY);
+
+        struct termios tty;
+        struct termios tty_old;
+        memset(&tty, 0, sizeof tty);
+
+        tty_old = tty;
+
+        cfsetospeed (&tty, (speed_t)baudRate);
+        cfsetispeed (&tty, (speed_t)baudRate);
+
+        tty.c_cflag &= ~PARENB;        // Make 8n1
+        tty.c_cflag     &=  ~CSTOPB;
+        tty.c_cflag     &=  ~CSIZE;
+        tty.c_cflag     |=  CS8;
+
+        tty.c_cflag     &=  ~CRTSCTS;       // no flow control
+        tty.c_cc[VMIN]      =   1;                  // read doesn't block
+        tty.c_cc[VTIME]     =   5;                  // 0.5 seconds read timeout
+        tty.c_cflag     |=  CREAD | CLOCAL;     // turn on READ & ignore ctrl lines
+
+        cfmakeraw(&tty);
+
+        tcflush(serial, TCIFLUSH );
+    }
+
+	void writeSerial(unsigned char r, unsigned char g, unsigned char b) {
+		unsigned char c[4];
+		c[0] = 0xFF;
+		c[1] = r;
+		c[2] = g;
+		c[3] = b;
+        write(serial, c, sizeof(c));
+	}
+
+private:
+    int serial;
+};
+
+#endif
