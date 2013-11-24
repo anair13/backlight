402430403a63ab0bc26fcd90df6ac953c49fb6d5
unknown <saavan@berkeley.edu>
2013-11-24 01:57:05 -0800

    added stereo to mono library

---
+++ stereo_to_mono.py
@@ -0,0 +1,15 @@
+import wave
+
+source = "music/pianoc.wav"
+destination = source[:-4] + "_mono.wav"
+
+source, destination = wave.open(source, "r"), wave.open(destination, "w")
+
+destination.setnchannels(1)
+destination.setsampwidth(source.getsampwidth())
+destination.setframerate(source.getframerate())
+destination.setcomptype(source.getcomptype(),source.getcompname())
+destination.writeframes(source.readframes(source.getnframes()))
+
+destination.close()
+source.close()
