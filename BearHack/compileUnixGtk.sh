f8624287125b2c6e813576a66687e8eb54cc280f
Roy Tu <thatsnail@gmail.com>
2014-08-21 12:20:31 -0700

    Compiles to bin

--- BearHack/compileUnixGtk.sh
+++ BearHack/compileUnixGtk.sh
@@ -1,1 +1,1 @@
-g++ -w -g -Wall -Iinclude src/main_gtk.cpp -o BearHack `pkg-config gtkmm-3.0 --cflags --libs` -Llib -lboost_system
+g++ -w -g -Wall -Iinclude src/main_gtk.cpp -o bin/BearHack `pkg-config gtkmm-3.0 --cflags --libs` -Llib -lboost_system
--- BearHack/main_kde.pro
+++ BearHack/main_kde.pro
@@ -1,4 +1,5 @@
 INCLUDEPATH += ./include
+TARGET = bin/BearHack
 LIBS += -Llib -lboost_system
 TEMPLATE += app
 QT += gui 
