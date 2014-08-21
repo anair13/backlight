c4266f15974c2dc3a0d45b54cbf5f361db7b2897
Roy Tu <thatsnail@gmail.com>
2014-08-21 11:45:08 -0700

    Cleanup

--- BearHack/compileUnixGtk.sh
+++ BearHack/compileUnixGtk.sh
@@ -1,2 +1,1 @@
-#g++ -w -g -Wall -Iinclude main.cpp -o BearHack `pkg-config gtkmm-3.0 --cflags --libs` -lboost_system-mgw45-mt-1_53 -lboost_date_time-mgw45-mt-1_53 -lboost_regex-mgw45-mt-1_53
 g++ -w -g -Wall -Iinclude main_gtk.cpp -o BearHack `pkg-config gtkmm-3.0 --cflags --libs` -Llib -lboost_system
