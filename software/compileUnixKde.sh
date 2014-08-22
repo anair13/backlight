9bd11188f30ee7c3c38fe3f2689cf5809e841f7d
Roy Tu <thatsnail@gmail.com>
2014-08-22 01:58:06 -0700

    Compile music_kde.cpp

--- software/compileUnixKde.sh
+++ software/compileUnixKde.sh
@@ -1,2 +1,3 @@
-qmake QMAKE_CXXFLAGS_WARN_ON=-w QMAKE_CFLAGS_WARN_ON=-w && make
-rm main_kde.o Makefile
+#qmake QMAKE_CXXFLAGS_WARN_ON=-w QMAKE_CFLAGS_WARN_ON=-w && make
+#rm main_kde.o Makefile
+g++ -w -g -Wall -Iinclude -Isrc/kiss_fft130 src/kiss_fft130/kiss_fft.c src/music_kde.cpp -o bin/MusicLight -Llib -lboost_system -lpulse -lpulse-simple
